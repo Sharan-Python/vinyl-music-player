a75e1296b10ed2b04586a7433ff2e5add90d9778
Sharan-Python <75255241+Sharan-Python@users.noreply.github.com>
2021-01-25 19:05:07 +0530

    Edit Popup

--- Vinyl.py
+++ Vinyl.py
@@ -33,7 +33,7 @@ class Popup(Toplevel):
 
 def about():
     Popup("About Us",
-          "Powered By Python \n Version 1.0 \n Problems? Feel free to create an issue on Github \n Do fork your own version...")
+          "Powered By Python \n Version 1.0 \n Problems? Feel free to create an issue on Github \n Do fork your own version... to contribute")
 
 
 def set_vol(val):
